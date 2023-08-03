b78b2a06a78852a3fb4ad383a9356c556aa171ed
aditi27-rajshail <aditirajankhot.7@gmail.com>
2023-08-03 11:09:19 +0530

    invoice format aded

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -1,26 +1,279 @@
 import React, { FunctionComponent } from "react";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import { css } from "@emotion/core";
-import { CocTemplateCertificate } from "../samples/customTemplate";
+import { InvoiceTemplateCertificate } from "../samples/customTemplate";
+import { documentTemplates } from "@govtechsg/decentralized-renderer-react-components/build/types/utils";
 
 const containerStyle = css`
-  background-color: #324353;
-  color: #ffffff;
-  padding: 15px;
+  padding: 10px;
   margin: auto;
-  width: 80%;
+  width: 100%;
+`;
+
+const rowStyle = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const innerContainer = css`
+  border: 1px solid #000;
+`;
+
+const fourColumnsRowStyle = css`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+const singleRowStyle = css`
+  display: grid;
+  grid-template-columns: 1fr;
+`;
+
+const cellStyle = css`
+  border: 1px solid #000;
+  padding: 8px;
+
+  padding: 10px;
+  p {
+    margin: 0;
+    font-weight: bold;
+  }
+  h4 {
+    margin: 0;
+    font-weight: normal;
+  }
+`;
+
+const titleStyle = css`
   text-align: center;
 `;
 
-export const CocTemplate: FunctionComponent<TemplateProps<CocTemplateCertificate> & { className?: string }> = ({
+const signatureStyle = css`
+  width: 100px;
+  height: auto;
+  display: block;
+  margin: 10px 0;
+`;
+
+const tableCellStyle = css`
+  border-left: 1px solid #000;
+  border-right: 1px solid #000;
+  padding: 8px;
+  text-align: left;
+`;
+
+const tableHeaderCellStyle = css`
+  border: 1px solid #000;
+  padding: 8px;
+  text-align: left;
+  font-weight: 800;
+`;
+
+const tableStyle = css`
+  width: 100%;
+  border-collapse: collapse;
+  & tr:first-child td {
+    border-top: 1px solid #000;
+  }
+  & tr:last-child td {
+    border-bottom: 2px solid #000;
+  }
+`;
+
+export const CocTemplate: FunctionComponent<TemplateProps<InvoiceTemplateCertificate> & { className?: string }> = ({
   document,
   className = ""
 }) => {
   return (
     <div css={containerStyle} className={className} id="custom-template">
-      <h1>{document.name}</h1>
-      <div>awarded to</div>
-      <h2>{document.recipient.name}</h2>
+      <h1 css={titleStyle}>COMMERCIAL INVOICE</h1>
+      <div css={innerContainer}>
+        <div css={rowStyle}>
+          <div css={cellStyle}>
+            <h4>Exporter</h4>
+            <p>{document.exporter.name}</p>
+            <p>{document.exporter.address}</p>
+            <p>{document.exporter.phoneNumber}</p>
+            <p>{document.exporter.email}</p>
+          </div>
+          <div css={cellStyle}>
+            <h4>Invoice Details</h4>
+            <p>Title: {document.title}</p>
+            <p>Number: {document.swiftCode}</p>
+            <p>{`Date: ${document.invoiceCreationDate}`}</p>
+            <p>Insurance Policy Number: {document.insurancePolicyNumber}</p>
+          </div>
+        </div>
+        <div css={rowStyle}>
+          <div css={cellStyle}>
+            <h4>Importer</h4>
+            <p>{document.importer.name}</p>
+            <p>{document.importer.address}</p>
+            <p>{document.importer.phoneNumber}</p>
+            <p>{document.importer.email}</p>
+          </div>
+          <div css={rowStyle}>
+            <div css={cellStyle}>
+              <h4>Bill of Ladding Number</h4>
+              <p>{document.blNumber}</p>
+            </div>
+            <div css={cellStyle}>
+              <h4>Bill of Ladding Date</h4>
+              <p>{document.blDate}</p>
+            </div>
+            <div css={cellStyle}>
+              <h4>Buyer Contract</h4>
+              <p>{document.buyerContract}</p>
+            </div>
+            <div css={cellStyle}>
+              <h4>LC Reference No.</h4>
+              <p>{document.lcRefNumber}</p>
+            </div>
+          </div>
+        </div>
+        <div css={rowStyle}>
+          <div css={rowStyle}>
+            <div css={cellStyle}>
+              <h4>Method of Dispatch</h4>
+              <p>{document.modeOfDispatch}</p>
+            </div>
+            <div css={cellStyle}>
+              <h4>Vessel Name</h4>
+              <p>{document.vesselName}</p>
+            </div>
+            <div css={cellStyle}>
+              <h4>Port of Loading</h4>
+              <p>{document.portOfLoading}</p>
+            </div>
+            <div css={cellStyle}>
+              <h4>Port of Discharge</h4>
+              <p>{document.portOfDischarge}</p>
+            </div>
+          </div>
+          <div css={cellStyle}>
+            <h4>Terms / Method of Payment</h4>
+            <p>{document.paymentTerms}</p>
+            <p>{document.paymentMethod}</p>
+          </div>
+        </div>
+
+        <div css={fourColumnsRowStyle}>
+          <div css={cellStyle}>
+            <h4>Country of Origin</h4>
+            <p>{document.countryOfOrigin}</p>
+          </div>
+          <div css={cellStyle}>
+            <h4>Country of Destination</h4>
+            <p>{document.countryOfDestination}</p>
+          </div>
+          <div css={cellStyle}>
+            <h4>Incoterms</h4>
+            <p>{document.incoterms}</p>
+          </div>
+          <div css={cellStyle}>
+            <h4>Is Electronic Invoice</h4>
+            <p>{document.isElectronicInvoice}</p>
+          </div>
+        </div>
+        <div css={singleRowStyle}>
+          <table css={[cellStyle, tableStyle]}>
+            <tr>
+              <td css={tableHeaderCellStyle}>HS Code</td>
+              <td css={tableHeaderCellStyle}>Commodity Name</td>
+              <td css={tableHeaderCellStyle}>Commodity Description</td>
+              <td css={tableHeaderCellStyle}>Gross weight (KG)</td>
+              <td css={tableHeaderCellStyle}>Moisture Content %</td>
+              <td css={tableHeaderCellStyle}>Qty</td>
+              <td css={tableHeaderCellStyle}>Unit Price</td>
+              <td css={tableHeaderCellStyle} style={{ width: "150px" }}>
+                Total Price
+              </td>
+            </tr>
+            {document.commodity.map((commodity, index) => (
+              <tr key={index}>
+                <td css={tableCellStyle}>{commodity.hsCode}</td>
+                <td css={tableCellStyle}>{commodity.name}</td>
+                <td css={tableCellStyle}>{commodity.description}</td>
+                <td css={tableCellStyle}>{commodity.grossWeight}</td>
+                <td css={tableCellStyle}>{commodity.moistureContent}</td>
+                <td css={tableCellStyle}>{commodity.qty}</td>
+                <td css={tableCellStyle}>{commodity.unitPrice}</td>
+                <td css={tableCellStyle}>{commodity.unitPrice * commodity.qty}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+
+        <div css={rowStyle}>
+          <div css={cellStyle}>
+            <h4>Additional Information</h4>
+            <p>{document.additionalInformation}</p>
+          </div>
+
+          <table css={[cellStyle, tableStyle]}>
+            <tr>
+              <td css={tableHeaderCellStyle} style={{ textAlign: "right" }}>
+                Total Commercial Value
+              </td>
+              <td css={tableHeaderCellStyle}>
+                {document.commodity.reduce((acc, commodity) => acc + commodity.unitPrice * commodity.qty, 0)}
+              </td>
+            </tr>
+            <tr>
+              <td css={tableHeaderCellStyle} style={{ textAlign: "right" }}>
+                Misc. Charges (packing, shipping, etc.) (+)
+              </td>
+              <td css={tableHeaderCellStyle} style={{ width: "150px" }}>
+                {document.miscCharges}
+              </td>
+            </tr>
+            <tr>
+              <td css={tableHeaderCellStyle} style={{ textAlign: "right" }}>
+                Credit Amount (-)
+              </td>
+              <td css={tableHeaderCellStyle} style={{ width: "150px" }}>
+                {document.creditAmount}
+              </td>
+            </tr>
+            <tr>
+              <td css={tableHeaderCellStyle} style={{ textAlign: "right" }}>
+                Total Invoice Value
+              </td>
+              <td css={tableHeaderCellStyle}>
+                {document.commodity.reduce((acc, commodity) => acc + commodity.unitPrice * commodity.qty, 0) +
+                  document?.miscCharges -
+                  document?.creditAmount}
+              </td>
+            </tr>
+            <tr>
+              <td css={tableHeaderCellStyle} style={{ textAlign: "right" }}>
+                Payment Due Date
+              </td>
+              <td css={tableHeaderCellStyle}>{document.paymentDueDate}</td>
+            </tr>
+          </table>
+        </div>
+        <div css={rowStyle}>
+          <div css={cellStyle}>
+            <h4>Bank Details</h4>
+            <p>{`Benefiaciary's Bank Name: ${document.bankName}`}</p>
+            <p>{`Benefiaciary's Name: ${document.importer.name}`}</p>
+            <p>Bank Account Number: {document.bankAccountNumber}</p>
+            <p>Swift Code: {document.swiftCode}</p>
+          </div>
+          <div css={cellStyle}>
+            <h4>I certify that state export prices and description of goods are true and correct</h4>
+            <img
+              css={signatureStyle}
+              src="https://upload.wikimedia.org/wikipedia/commons/1/19/Victoria_Justice_Signature.png"
+              alt="Signature"
+            />
+            <p>Name of Authorised Signatory: {document.exporter.name}</p>
+            <p>Place of Issue: {document.placeOfIssue}</p>
+            <p>{`Date: ${document.invoiceCreationDate}`}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/templates/samples/customTemplate.ts
+++ src/templates/samples/customTemplate.ts
@@ -1,14 +1,75 @@
 import { v2 } from "@govtechsg/open-attestation";
 
-export interface CocTemplateCertificate extends v2.OpenAttestationDocument {
+interface ComodityInterface {
+  hsCode: string;
   name: string;
-  recipient: {
+  description: string;
+  grossWeight: number;
+  moistureContent: number;
+  qty: number;
+  unitPrice: number;
+}
+export interface InvoiceTemplateCertificate extends v2.OpenAttestationDocument {
+  exporter: {
     name: string;
+    address: string;
+    phoneNumber: string;
+    email: string;
   };
+
+  title: string;
+  invoiceNumber: string;
+  invoiceCreationDate: string;
+  insurancePolicyNumber: string;
+
+  importer: {
+    name: string;
+    address: string;
+    phoneNumber: string;
+    email: string;
+  };
+
+  blNumber: string;
+  blDate: string;
+
+  buyerContract: string;
+  lcRefNumber: string;
+
+  modeOfDispatch: string;
+  vesselName: string;
+  portOfLoading: string;
+  portOfDischarge: string;
+  countryOfOrigin: string;
+  countryOfDestination: string;
+
+  paymentMethod: string;
+  paymentTerms: string;
+
+  incoterms: string;
+  isElectronicInvoice: string;
+
+  commodity: ComodityInterface[];
+
+  additionalInformation: string;
+
+  miscCharges: number;
+  creditAmount: number;
+  paymentDueDate: string;
+
+  signature: string;
+  placeOfIssue: string;
+
+  bankAccountNumber: string;
+  bankName: string;
+  swiftCode: string;
 }
 
-export const cocTemplateCertificate: CocTemplateCertificate = {
-  name: "OpenAttestation Tutorial Certificate of Completion",
+export const invoiceTemplateCertificate: InvoiceTemplateCertificate = {
+  $template: {
+    name: "COC",
+    type: v2.TemplateType.EmbeddedRenderer,
+    url: "http://localhost:3000"
+  },
   issuers: [
     {
       name: "My name",
@@ -19,12 +80,81 @@ export const cocTemplateCertificate: CocTemplateCertificate = {
       }
     }
   ],
-  recipient: {
-    name: "John Doe"
+
+  exporter: {
+    name: "Exporter 1",
+    address: "Exporter Address 1",
+    phoneNumber: "+1-555-555-5555",
+    email: "exporter1@example.com"
   },
-  $template: {
-    name: "COC",
-    type: v2.TemplateType.EmbeddedRenderer,
-    url: "http://localhost:3000"
-  }
+
+  title: "Sample Invoice",
+  invoiceNumber: "INV123456789",
+  invoiceCreationDate: "2023-08-01",
+  insurancePolicyNumber: "IPN987654321",
+
+  importer: {
+    name: "Importer 1",
+    address: "Importer Address 1",
+    phoneNumber: "+1-555-555-5556",
+    email: "importer1@example.com"
+  },
+
+  blNumber: "BL123456789",
+  blDate: "2023-08-01",
+  buyerContract: "Contract 1",
+  lcRefNumber: "LCREF123456",
+
+  modeOfDispatch: "Sea",
+  vesselName: "Vessel 1",
+  portOfDischarge: "Port 1",
+  portOfLoading: "Port 2",
+  countryOfOrigin: "Country 1",
+  countryOfDestination: "Country 2",
+
+  paymentMethod: "Online",
+  paymentTerms: "30 Days",
+  incoterms: "Incoterm 1",
+  isElectronicInvoice: "Yes",
+
+  commodity: [
+    {
+      hsCode: "HS123456",
+      name: "Commodity 1",
+      description: "Description for Commodity 1",
+      grossWeight: 500,
+      moistureContent: 10,
+      qty: 100,
+      unitPrice: 5
+    },
+    {
+      hsCode: "HS123456",
+      name: "Commodity 2",
+      description: "Description for Commodity 2",
+      grossWeight: 500,
+      moistureContent: 10,
+      qty: 100,
+      unitPrice: 5
+    },
+    {
+      hsCode: "HS123456",
+      name: "Commodity 3",
+      description: "Description for Commodity 3",
+      grossWeight: 500,
+      moistureContent: 10,
+      qty: 100,
+      unitPrice: 5
+    }
+  ],
+
+  additionalInformation: "Additional Information",
+  miscCharges: 100,
+  creditAmount: 500,
+  paymentDueDate: "2023-08-31",
+  bankAccountNumber: "1234567890",
+  bankName: "Sample Bank",
+  swiftCode: "SAMPLESWIFT",
+
+  signature: "https://www.pngegg.com/en/png-zumpd",
+  placeOfIssue: "Sample Place"
 };
