ced785833dd45269124cafdd32651f56aadf9984
aditi27-rajshail <aditirajankhot.7@gmail.com>
2023-11-21 13:12:38 +0530

    YM-289, YM-288 Template label fixes

--- src/templates/billOfLading/BillOfLadingTemplate.tsx
+++ src/templates/billOfLading/BillOfLadingTemplate.tsx
@@ -244,13 +244,13 @@ export const BillOfLadingTemplate: FunctionComponent<TemplateProps<BLTemplateCer
             </div>
             <div css={cellStyle}>
               <h4>Payment Terms / Method of Payment</h4>
-              <p>{document.paymentTerms} Days</p>
+              {document.paymentTerms && <p>{document.paymentTerms} Days</p>}
               <p>{document.paymentMethod}</p>
             </div>
           </div>
 
           <div css={fourColumnsRowStyle}>
-            <div css={cellStyle}>
+            {/* <div css={cellStyle}>
               <h4>City & Country of Origin</h4>
               <p>
                 {document.cityOfOrigin}, {document.countryOfOrigin}
@@ -261,7 +261,22 @@ export const BillOfLadingTemplate: FunctionComponent<TemplateProps<BLTemplateCer
               <p>
                 {document.cityOfDestination}, {document.countryOfDestination}
               </p>
+            </div> */}
+            <div css={cellStyle}>
+              <h4>City & Country of Origin</h4>
+              <p>
+                {document.cityOfOrigin ? `${document.cityOfOrigin}, ` : ""}
+                {document.countryOfOrigin}
+              </p>
             </div>
+            <div css={cellStyle}>
+              <h4>City & Country of Destination</h4>
+              <p>
+                {document.cityOfDestination ? `${document.cityOfDestination}, ` : ""}
+                {document.countryOfDestination}
+              </p>
+            </div>
+
             <div css={cellStyle}>
               <h4>Incoterms</h4>
               <p>{document.incoterms}</p>
@@ -320,7 +335,9 @@ export const BillOfLadingTemplate: FunctionComponent<TemplateProps<BLTemplateCer
                   <td css={tableCellStyle}>{singlePackage.name}</td>
                   <td css={tableCellStyle}>{singlePackage.marksAndNo}</td>
                   <td css={tableCellStyle}>
-                    {singlePackage.type} X {singlePackage.noOfPackage}
+                    {singlePackage.type || singlePackage.noOfPackage
+                      ? `${singlePackage.type} X ${singlePackage.noOfPackage}`
+                      : ""}
                   </td>
                   <td css={tableCellStyle}>{singlePackage.description}</td>
                   <td css={tableCellStyle}>
